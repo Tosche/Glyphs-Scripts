7f00e05b0629df028dfbfa0a294044dffadd530e
Tosche <toshi@Toshis-MacBook-Air-2.local>
2014-12-17 12:06:55 +0000

    Bug fix: Export Tagged Text script
    Fixed bug in the Export Tagged Text Script:
    - It now uses the instance properly.
    - It counts only the exported glyphs.

--- Export Tagged Text for InDesign.py
+++ Export Tagged Text for InDesign.py
@@ -12,28 +12,37 @@ thisFontMaster = thisFont.selectedFontMaster # active master
 selectedLayers = thisFont.selectedLayers # active layers of selected glyphs
 thisDoc = Glyphs.currentDocument
 
-paraStyleName = thisFont.familyName + " 12pt"
+
 
 instanceList = []
 for thisInstance in thisFont.instances:
 	if thisInstance.active:
 		instanceList.append(thisInstance.name)
 
-line = ""
-for i in range(len(thisFont.glyphs)):
-	line += "<ParaStyle:%s><cSpecialGlyph:%s><0xFFFD>" % (styleName, i+1)
+glyphCount = 0
+for glyph in thisFont.glyphs:
+	if glyph.export:
+		glyphCount += 1
 
-for item in instanceList:
-	header = "<ASCII-MAC>\n<Version:7.5><FeatureSet:InDesign-Roman><ColorTable:=<Black:COLOR:CMYK:Process:0,0,0,1>>\n<DefineParaStyle:%s=<cSize:12.000000>%s<cFont:%s><cOTFContAlt:0>>\n" % (paraStyleName, item, thisFont.familyName)
+
+for instance in instanceList:
+	paraStyleName = "%s %s 12pt" % (thisFont.familyName, instance)
+
+	header = "<ASCII-MAC>\n<Version:7.5><FeatureSet:InDesign-Roman><ColorTable:=<Black:COLOR:CMYK:Process:0,0,0,1>>\n<DefineParaStyle:%s=<cSize:12.000000><cTypeface:%s><cFont:%s><cOTFContAlt:0>>\n" % (paraStyleName, instance, thisFont.familyName)
+
+	line = ""
+	for i in range(glyphCount-1):
+		line += "<ParaStyle:%s><cSpecialGlyph:%s><0xFFFD>" % (paraStyleName, i+1)
+
 	try:
 		directory = "the same folder as the working file"
 		dirPath = os.path.dirname(thisFont.filepath)
 	except:
 		directory = "the Documents folder, because the source is not a saved Glyphs file"
 		dirPath = filepath = abspath(expanduser("~/") + '/Documents')
-	filePath = abspath(dirPath+"/Tagged Text - %s %s.txt" % (thisFont.familyName, item))
+	filePath = abspath(dirPath+"/Tagged Text - %s %s.txt" % (thisFont.familyName, instance))
 	with open(filePath, 'w') as thisFile:
 		thisFile.write(header+line)
 		thisFile.close
 
-Glyphs.displayDialog_(u'Tagged text file(s) saved in %s. Please follow the instruction:\n\n1. Generate the font and make it available in InDesign (okay to do it before running this script).\n\n2. In InDesign, have a document open. Go to "File > Place", choose the generated text file, and place it somewhere in the document.\n\n3. Voilà! Now you have all glyphs, referred to in Glyph ID.' % directory)
+Glyphs.displayDialog_(u'Tagged text files saved in %s (overwitten existing tagged texts if they exist). Please follow the instruction:\n\n1. Generate the font and make it available in InDesign (okay to do it before running this script).\n\n2. In InDesign, have a document open. Go to "File > Place", choose the generated text file, and place it somewhere in the document.\n\n3. Voilà! Now you have all glyphs, referred to in Glyph ID.' % directory)
