0bae4444d7b3365cc4c6d19acb3ce6952bb50df8
Toshi Omagari <toshi@toshis-air.home>
2016-03-26 18:51:39 +0000

    Bugfix Delete Diagonal Nodes

--- Path/Delete Diagonal Nodes Between Extremes.py
+++ Path/Delete Diagonal Nodes Between Extremes.py
@@ -21,13 +21,14 @@ def process( thisLayer ):
 				hNode = thisPath.nodes[i-1]
 				iNode = thisPath.nodes[i]
 				jNode = thisPath.nodes[i+1]
-				if iNode.type != GSCURVE: #if thisNode is on-curve
-					if hNode.type == GSCURVE and jNode.type==GSCURVE:
-						#diagonal cleaner
+				if iNode.type != GSOFFCURVE: #if thisNode is on-curve
+					if hNode.type == GSOFFCURVE and jNode.type == GSOFFCURVE:
+						# on-curve now found
+						# diagonal cleaner
 						try:
-							if thisPath.nodes[i+3].x == thisPath.nodes[i+4].x or thisPath.nodes[i+3].y == thisPath.nodes[i+4].y or thisPath.nodes[i-3].x == thisPath.nodes[i-4].x or thisPath.nodes[i-3].y == thisPath.nodes[i-4].y:
+							if thisPath.nodes[i+2].x == thisPath.nodes[i+3].x or thisPath.nodes[i+2].y == thisPath.nodes[i+3].y or thisPath.nodes[i-2].x == thisPath.nodes[i-3].x or thisPath.nodes[i-2].y == thisPath.nodes[i-3].y:
 								if hNode.x == jNode.x or hNode.y == jNode.y:
-									pass
+									pass # because the node is extreme
 								else:
 									boolx1 = hNode.x < iNode.x < jNode.x
 									boolx2 = hNode.x > iNode.x > jNode.x
@@ -40,25 +41,6 @@ def process( thisLayer ):
 											thisPath.removeNodeCheckKeepShape_(iNode)
 						except:
 							pass
-
-						#diagonal cleaner, exactly the same again
-						try:
-							if thisPath.nodes[i+3].x == thisPath.nodes[i+4].x or thisPath.nodes[i+3].y == thisPath.nodes[i+4].y or thisPath.nodes[i-3].x == thisPath.nodes[i-4].x or thisPath.nodes[i-3].y == thisPath.nodes[i-4].y:
-								if hNode.x == jNode.x or hNode.y == jNode.y:
-									pass
-								else:
-									boolx1 = hNode.x < iNode.x < jNode.x
-									boolx2 = hNode.x > iNode.x > jNode.x
-									booly1 = hNode.y < iNode.y < jNode.y
-									booly2 = hNode.y > iNode.y > jNode.y
-									if (boolx1 or boolx2) and (booly1 or booly2):
-										atan2hi = math.atan2(iNode.y-hNode.y,iNode.x-hNode.x)
-										atan2ij = math.atan2(jNode.y-iNode.y,jNode.x-iNode.x)
-										if abs(atan2ij-atan2hi) < 0.1:
-											thisPath.removeNodeCheckKeepShape_(iNode)
-						except:
-							pass
-
 			except:
 				pass
 
@@ -71,6 +53,7 @@ for thisLayer in listOfSelectedLayers:
 		layer = thisGlyph.layers[thisMaster.id]
 		layer.setBackground_(layer)
 		process( layer )
+		process( layer ) # run the process again, just to make sure
 	thisGlyph.endUndo()   # end undo grouping
 
 thisFont.enableUpdateInterface() # re-enables UI updates in Font View
