b2e6427cdf5ce60cd738288c51040be736645446
Toshi Omagari <tosche@mac.com>
2020-12-03 01:30:48 +0000

    Update Delete Diagonal Nodes Between Extremes.py

--- Path/Delete Diagonal Nodes Between Extremes.py
+++ Path/Delete Diagonal Nodes Between Extremes.py
@@ -13,39 +13,42 @@ sel = f.selectedLayers # active layers of selected glyphs
 
 def deleteDiagonals( thisLayer ):
 	for pathindex, p in enumerate(thisLayer.paths):
-		numOfNodes = len(p.nodes)
-		for i in range( -1, numOfNodes):
+		for nodeindex, n in enumerate(p.nodes):
 			try:
-				hNode = p.nodes[i-1]
-				iNode = p.nodes[i]
-				jNode = p.nodes[i+1]
-				if iNode.type != GSOFFCURVE: #if thisNode is on-curve
-					if hNode.type == GSOFFCURVE and jNode.type == GSOFFCURVE:
-						# on-curve now found
-						# diagonal cleaner
+				nPrev1 = n.prevNode
+				nPrev2 = nPrev1.prevNode
+				nPrev3 = nPrev2.prevNode
+				nNext1 = n.nextNode
+				nNext2 = nNext1.nextNode
+				nNext3 = nNext2.nextNode
+
+				if n.type != GSOFFCURVE: #if thisNode is on-curve
+					if nPrev1.type == GSOFFCURVE and nNext1.type == GSOFFCURVE: # if n is sandwiched by off-curves
 						try:
-							if p.nodes[i+2].x == p.nodes[i+3].x or p.nodes[i+2].y == p.nodes[i+3].y or p.nodes[i-2].x == p.nodes[i-3].x or p.nodes[i-2].y == p.nodes[i-3].y:
-								if hNode.x == jNode.x or hNode.y == jNode.y:
-									pass # because the node is extreme
+							if nNext2.x == nNext3.x or nNext2.y == nNext3.y or nPrev2.x == nPrev3.x or nPrev2.y == nPrev3.y:
+								if nPrev1.x == nNext1.x or nPrev1.y == nNext1.y:
+									pass # because n is extreme
 								else:
-									boolx1 = hNode.x < iNode.x < jNode.x
-									boolx2 = hNode.x > iNode.x > jNode.x
-									booly1 = hNode.y < iNode.y < jNode.y
-									booly2 = hNode.y > iNode.y > jNode.y
+									boolx1 = nPrev1.x < n.x < nNext1.x
+									boolx2 = nPrev1.x > n.x > nNext1.x
+									booly1 = nPrev1.y < n.y < nNext1.y
+									booly2 = nPrev1.y > n.y > nNext1.y
 									if (boolx1 or boolx2) and (booly1 or booly2):
-										atan2hi = math.atan2(iNode.y-hNode.y,iNode.x-hNode.x)
-										atan2ij = math.atan2(jNode.y-iNode.y,jNode.x-iNode.x)
+										atan2hi = math.atan2(n.y-nPrev1.y,n.x-nPrev1.x)
+										atan2ij = math.atan2(nNext1.y-n.y,nNext1.x-n.x)
+
+										# check if n is an inflection point
 										if abs(atan2ij-atan2hi) < 0.1:
 											dupLayer = thisLayer.copy()
 											dupPath = dupLayer.paths[pathindex]
-											dupNode = dupPath.nodes[i]
+											dupNode = dupPath.nodes[nodeindex]
 											dupPath.removeNodeCheckKeepShape_(dupNode)
 											nodesBefore = len(dupPath.nodes)
 											dupLayer.addInflectionPoints()
 											nodesAfter = len(dupPath.nodes)
 											
 											if (nodesBefore == nodesAfter):
-												p.removeNodeCheckKeepShape_(iNode)
+												p.removeNodeCheckKeepShape_(n)
 						except:
 							pass
 			except:
@@ -57,8 +60,8 @@ for l in sel:
 	g = l.parent
 	g.beginUndo() # begin undo grouping
 	l.setBackground_(l)
-	deleteDiagonals( l )
-	deleteDiagonals( l ) # run the process again, just to make sure
+	deleteDiagonals(l)
+	deleteDiagonals(l) # run the process again, just to make sure
 	g.endUndo()   # end undo grouping
 
 f.enableUpdateInterface() # re-enables UI updates in Font View
